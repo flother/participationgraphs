a0aff9b8b502e2849fe61d147f8b1b4d9294afb8
Matt <matt.riggott@gmail.com>
2010-06-08 21:30:25 +0000

    Added setup.py so the repository can be installed as an egg via PIP.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="participationgraphs",
+    version=__import__("participationgraphs").__version__,
+    author="Matt Riggott",
+    description="Django app with a template tag to allow you to include sparklines of the 52-week commit history for a project on Github",
+    long_description=open("README").read(),
+    license="GPL",
+    url="http://github.com/flother/participationgraphs",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
